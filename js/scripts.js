ba2ef74c6d5300c26c574fe8602ebf265b7105f6
minhparis <36161599+minhparis@users.noreply.github.com>
2021-03-03 23:31:12 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,80 @@
+(function($) {
+    "use strict"; 
+	
+	/* Preloader */
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 500);
+		}
+		hidePreloader();
+	});
+
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+    /* Card Slider - Swiper */
+	var cardSlider = new Swiper('.card-slider', {
+		autoplay: {
+            delay: 4000,
+            disableOnInteraction: false
+		},
+        loop: true,
+        navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+		},
+		slidesPerView: 3,
+		spaceBetween: 20,
+        breakpoints: {
+            // when window is <= 992px
+            992: {
+                slidesPerView: 2
+            },
+            // when window is <= 768px
+            768: {
+                slidesPerView: 1
+            } 
+        }
+    });
+
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+    });
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
+    });
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+    
+})(jQuery);
